8bf3f74c4b7f63478966b606607099b434693c49
Kavindu <156315964+KaviDJ02@users.noreply.github.com>
2026-02-05 00:33:01 +0530

    chore: simplify babel config and update tailwind content paths; add global type definitions

--- babel.config.js
+++ babel.config.js
@@ -6,9 +6,20 @@ module.exports = function (api) {
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
     ],
     plugins: [
-      'nativewind/babel',       
+      'nativewind/babel',
       'expo-router/babel',
       'react-native-worklets/plugin',
     ],
   };
 };
+
+
+// module.exports = function (api) {
+//   api.cache(true);
+
+//   return {
+//     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+//     plugins: ['react-native-worklets/plugin'],
+//   };
+// };
+
